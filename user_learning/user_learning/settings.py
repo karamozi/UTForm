ad7acadd90c4297c742f44825ca6409fafd0f58a
karamozi <88131225+karamozi@users.noreply.github.com>
2021-07-29 12:16:36 +0430

    Update settings.py

--- user_learning/user_learning/settings.py
+++ user_learning/user_learning/settings.py
@@ -123,7 +123,7 @@ STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ( BASE_DIR.joinpath('static'), )
+STATICFILES_DIRS = [ BASE_DIR.joinpath('static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
